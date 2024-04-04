edca9781e5c1399765e1f4d438ef2e271802b6b3
alireza <alireza.abedini78@gmail.com>
2024-04-04 02:09:48 -0400

    loss functions completed and documentation added

--- loss_functions.py
+++ loss_functions.py
@@ -1,7 +1,18 @@
 import torch
 import torch.nn.functional as functions
 
+
 def KL(alpha, c):
+    """
+    computes the Kullback-Leibler divergence between two Dirichlet distributions.
+    beta: a tensor of ones that represents the parameters of the base Dirichlet distribution.
+    alpha: sum of the alpha parameters for each observation (row in the alpha tensor).
+    S_beta: corresponding sum for the beta tensor.
+    lnB: log of the multivariate Beta function for the alpha parameters.
+    lnB_uni: log of the multivariate Beta function for the beta parameters (the uniform distribution).
+    dg0 and dg1: digamma function for S_alpha and alpha
+    """
+
     beta = torch.ones((1, c)).cuda()
     S_alpha = torch.sum(alpha, dim=1, keepdim=True)
     S_beta = torch.sum(beta, dim=1, keepdim=True)
@@ -13,4 +24,21 @@ def KL(alpha, c):
     return kl
 
 
+def ce_loss(p, alpha, c, global_step, annealing_step):
+    """
+    Overall loss for the TMC
+    S: sum of the alpha parameters (the strength of the Dirichlet distribution
+    E: expected value of the Dirichlet distribution
+    label: one-hot encodes the integer labels p into binary class matrix representations for multi-class classification.
+    """
+    S = torch.sum(alpha, dim=1, keepdim=True)
+    E = alpha - 1
+    label = functions.one_hot(p, num_classes=c)
+    A = torch.sum(label * (torch.digamma(S) - torch.digamma(alpha)), dim=1, keepdim=True)
 
+    annealing_coef = min(1, global_step / annealing_step)
+
+    alp = E * (1 - label) + 1
+    B = annealing_coef * KL(alp, c)
+
+    return (A + B)
