4167b49f4a8ad935e21d4a65db5fb51af2f03919
YaCnDehfuli <mryd71114@gmail.com>
2024-04-07 13:09:43 -0400

    Trainer class implemented and model file debugged

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import loss_functions
 import warnings
 warnings.filterwarnings("ignore")
@@ -15,8 +16,8 @@ class Classifier(nn.Module):
 
         self.fc = nn.ModuleList()
         for i in range(self.num_layers-1):
-            self.fc.append(nn.Linear(classifier_dims[i], classifier_dims[i]))
-        self.fc.append(nn.Linear(classifier_dims[self.num_layers], classes))
+            self.fc.append(nn.Linear(classifier_dims[i], classifier_dims[i+1]))
+        self.fc.append(nn.Linear(classifier_dims[self.num_layers-1], classes))
         self.fc.append(nn.Softplus())
 
     def forward(self, x):
@@ -30,12 +31,13 @@ class Classifier(nn.Module):
         return h
 
 
+
 class TMC(nn.Module):
     def __init__(self, classes, views, classifier_dims, lambda_epochs=1):
         """
         Main Trusted Multi-View Classification model, which integrates evidence from multiple views.
-
-        classes: Number of classes
+        
+        classes: Number of classes 
         views: Number of views
         classifier_dims: classifier Dimension
         annealing_epoch: KL divergence annealing epoch during training
@@ -57,9 +59,9 @@ class TMC(nn.Module):
         b, S, E, u = dict(), dict(), dict(), dict()
         for v in range(2):
             S[v] = torch.sum(alpha[v], dim=1, keepdim=True)
-            E[v] = alpha[v] - 1
-            b[v] = E[v] / (S[v].expand(E[v].shape))
-            u[v] = self.classes / S[v]
+            E[v] = alpha[v]-1
+            b[v] = E[v]/(S[v].expand(E[v].shape))
+            u[v] = self.classes/S[v]
 
         # b^0 @ b^(0+1)
         bb = torch.bmm(b[0].view(-1, self.classes, 1), b[1].view(-1, 1, self.classes))
@@ -75,15 +77,15 @@ class TMC(nn.Module):
         C = bb_sum - bb_diag
 
         # calculate b^a
-        b_a = (torch.mul(b[0], b[1]) + bu + ub) / ((1 - C).view(-1, 1).expand(b[0].shape))
+        b_a = (torch.mul(b[0], b[1]) + bu + ub)/((1-C).view(-1, 1).expand(b[0].shape))
         # calculate u^a
-        u_a = torch.mul(u[0], u[1]) / ((1 - C).view(-1, 1).expand(u[0].shape))
+        u_a = torch.mul(u[0], u[1])/((1-C).view(-1, 1).expand(u[0].shape))
 
         # calculate new S
         S_a = self.classes / u_a
         # calculate new e_k
         e_a = torch.mul(b_a, S_a.expand(b_a.shape))
-        alpha_a = e_a
+        alpha_a = e_a + 1
         return alpha_a
 
     def DS_Combin(self, alpha):
@@ -94,18 +96,18 @@ class TMC(nn.Module):
         alpha:  Dirichlet distribution parameters.
         return: Combined Dirichlet distribution parameters.
         """
-        for v in range(len(alpha) - 1):
-            if v == 0:
+        for v in range(len(alpha)-1):
+            if v==0:
                 alpha_a = self.DS_Combin_two(alpha[0], alpha[1])
             else:
-                alpha_a = self.DS_Combin_two(alpha_a, alpha[v])
+                alpha_a = self.DS_Combin_two(alpha_a, alpha[v+1])
         return alpha_a
 
     def forward(self, X, y, global_step):
         """
         The main method that processes multi-view input data (X), computes the evidence from each view, and aggregates it.
         """
-
+        
         evidence = self.infer(X)
         loss = 0
         alpha = dict()
@@ -117,7 +119,7 @@ class TMC(nn.Module):
 
         # Calculates the loss for each view using the custom ce_loss function from loss_functions
         # then combines the evidence and recalculates the loss for the aggregated evidence.
-
+        
         loss += loss_functions.ce_loss(y, alpha_a, self.classes, global_step, self.lambda_epochs)
         loss = torch.mean(loss)
         return evidence, evidence_a, loss
--- train.py
+++ train.py
@@ -1,6 +1,12 @@
+
+import argparse
 import torch
+import torch.optim as optim
 from model import TMC
 from data_loader import MultiViewData
+import warnings
+from torch.autograd import Variable
+warnings.filterwarnings("ignore")
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -20,3 +26,102 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
+
+class Trainer():
+    def __init__(self):
+        """
+        Initializes the trainer with hyperparameters like batch size, epochs, lambda for KL annealing, and learning rate.
+        Sets up the dataset and DataLoader for both training and testing phases.
+        Initializes the TMC model and the Adam optimizer.
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--batch-size', type=int, default=200, metavar='N',
+                            help='input batch size for training [default: 100]')
+        parser.add_argument('--epochs', type=int, default=500, metavar='N',
+                            help='number of epochs to train [default: 500]')
+        parser.add_argument('--lambda-epochs', type=int, default=50, metavar='N',
+                            help='gradually increase the value of lambda from 0 to 1')
+        parser.add_argument('--lr', type=float, default=0.0003, metavar='LR',
+                            help='learning rate')
+        self.args = parser.parse_args()
+        self.args.data_name = 'handwritten_6views'
+        self.args.data_path = 'datasets/' + self.args.data_name
+        self.args.dims = [[240], [76], [216], [47], [64], [6]]
+        self.args.views = len(self.args.dims)
+
+        self.train_loader = torch.utils.data.DataLoader(
+            MultiViewData(self.args.data_path, train=True), batch_size=self.args.batch_size, shuffle=True)
+        self.test_loader = torch.utils.data.DataLoader(
+            MultiViewData(self.args.data_path, train=False), batch_size=self.args.batch_size, shuffle=False)
+        N_mini_batches = len(self.train_loader)
+        print('The number of training images = %d' % N_mini_batches)
+
+        self.model = TMC(10, self.args.views, self.args.dims, self.args.lambda_epochs)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.args.lr, weight_decay=1e-5)
+        self.model.cuda()
+
+    def train(self, epoch):
+        """
+        Trains the TMC model on the data
+        """
+        # Sets the model to training mode.
+        self.model.train()
+        
+        # loss for each iteration is tracked using an AverageMeter instance
+        loss_meter = AverageMeter()
+        
+        # Iterates over batches of data, converting them to variables and moving them to the GPU.
+        for batch_idx, (data, target) in enumerate(self.train_loader):
+            for v_num in range(len(data)):
+                data[v_num] = Variable(data[v_num].cuda())
+            target = Variable(target.long().cuda())
+        
+            # refresh the optimizer
+            self.optimizer.zero_grad()
+            (self.optimizer.step()).evidences, evidence_a, loss = self.model(data, target, epoch)
+        
+            # The gradients are computed via backpropagation 
+            loss.backward()
+        
+            # Optimizer updates the model parameters 
+            self.optimizer.step()
+            loss_meter.update(loss.item())
+            
+    def test(self, epoch):
+        """
+        Returns the average loss and accuracy for the test set.
+        """
+        
+        # Sets the model to evaluation mode to disable dropout or batch normalization effects during inference.
+        self.model.eval()
+
+        # loss for each iteration is tracked using an AverageMeter instance
+        loss_meter = AverageMeter()
+        correct_num, data_num = 0, 0
+        for batch_idx, (data, target) in enumerate(self.test_loader):
+            for v_num in range(len(data)):
+                data[v_num] = Variable(data[v_num].cuda())
+            data_num += target.size(0)
+            
+            # Gradient computation disabled
+            with torch.no_grad():
+                target = Variable(target.long().cuda())
+                evidences, evidence_a, loss = self.model(data, target, epoch)
+                _, predicted = torch.max(evidence_a.data, 1)
+                
+                # Predictions are made, and the number of correct predictions is counted to calculate the accuracy.
+                correct_num += (predicted == target).sum().item()
+                loss_meter.update(loss.item())
+        return loss_meter.avg, correct_num/data_num
+
+    def run(self):
+        """
+        Perform the training process for the specified number of epochs.
+        """
+
+        for epoch in range(1, self.args.epochs + 1):
+            self.train(epoch)
+
+        # model is evaluated on the test set.
+        test_loss, acc = self.test(epoch)
+        return test_loss, acc
